45983ffb89b1aad7edac9037d8e3dfeec4dcac2a
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-09-06 16:20:32 -0700

    Basic blog post with props working

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class Post extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{this.props.title}</h1>
+        <h5>by {this.props.author}</h5>
+        <p>{this.props.body}</p>
+        <hr></hr>
+        <h2>Comments</h2>
+        <p>{this.props.comments}</p>
+
       </div>
     );
   }
--- react-blog/src/index.js
+++ react-blog/src/index.js
@@ -7,8 +7,15 @@ import registerServiceWorker from './registerServiceWorker';
 const post = {
 	title: "Dinosaurs Are Awesome",
 	author: "Stealthy Stegasaurus",
+	body: "Check out this hot body!",
+	comments: ["First", "Great Post!", "Love the bod!"]
 	
 }
 
-ReactDOM.render(<Post />, document.getElementById('root'));
+ReactDOM.render(<Post 
+	title={post.title}
+	author={post.author}
+	body={post.body}
+	comments={post.comments}
+	/>, document.getElementById('root'));
 registerServiceWorker();
